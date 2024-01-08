7225c7045244cf41285bcd6f48aae25bbabc2d5d
umberto <umberto.gherardi@gmail.com>
2024-01-08 10:44:48 -0700

    Add etch-a-sketch styling

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const grid = document.querySelector('.grid');
 
+const dimBtn = document.querySelector('button');
+dimBtn.addEventListener('click', () => {
+    let dimDivs = prompt("Enter the width/length for the grid: ");
+
+
+    document.removeChild(grid);
+});
+
+function removeGrid(dimDivs) {
+    for (let i = 0; i < dimDivs; i++) {
+        curRow = document.querySelector(".grid div");
+        console.log(curRow.target);
+        
+        gridRow.setAttribute('style', 'display: flex;')
+        
+        for (let j = 0; j < dimDivs; j++) {
+            let gridCell = document.createElement('div');
+        }
+    }   
+}
+
 function createGrid(dimDivs) {
     const DIM_PIXELS = 480;
     const cellDim = DIM_PIXELS / dimDivs;
@@ -13,11 +34,11 @@ function createGrid(dimDivs) {
         for (let j = 0; j < dimDivs; j++) {
             let gridCell = document.createElement('div');
             
-            gridCell.setAttribute('style', `height: ${cellDim}px; width: ${cellDim}px;`)
+            gridCell.setAttribute('style', `height: ${cellDim}px; width: ${cellDim}px; background-color: lightgray;`)
 
             gridCell.addEventListener('mouseover', function(e) {
                 console.log(e.target);
-                e.target.setAttribute('style', `background-color: red; height: ${cellDim}px; width: ${cellDim}px;`)
+                e.target.setAttribute('style', `background-color: gray; height: ${cellDim}px; width: ${cellDim}px;`)
             })
 
             gridRow.appendChild(gridCell);
@@ -25,7 +46,7 @@ function createGrid(dimDivs) {
         grid.appendChild(gridRow);
     }
 
-    grid.setAttribute('style', `border: 2px solid black;width: ${DIM_PIXELS}px;`)
+    grid.setAttribute('style', `border: 4px solid darkred; width: ${DIM_PIXELS}px;`)
 }
 
 createGrid(32);
